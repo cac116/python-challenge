5e6925fc8b8cc3ebde8f3dc09cd0e077221e2257
cac116 <cayans@gmail.com>
2018-05-05 21:35:41 -0500

    beta

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -4,6 +4,61 @@ import os
 #Import module to read CSVs
 import csv
 
+#state abreviations dictionary
+abbrev_dict = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
+
 # Specify the file to read from
 input_file = os.path.join('raw_data','employee_data1.csv')
 output_file = os.path.join('formated_employee_data1.csv')
@@ -23,6 +78,9 @@ last_name = []
 dob_year = []
 dob_month = []
 dob_day = []
+
+name_to_abbr = []
+abbrev = []
 
 with open(input_file, newline='') as csvfile:
 
@@ -50,10 +108,14 @@ for i in range(len(full_name)):
     dob_year.append(dob[i].split("-")[0])
     dob_month.append(dob[i].split("-")[1])
     dob_day.append(dob[i].split("-")[2])
-
     formated_dob.append(f"{dob_month[i]}/{dob_day[i]}/{dob_year[i]}")
-
     ssn[i]= ssn[i].replace(ssn[i][0:6],"***-**")
+        
+    for key in abbrev_dict.keys():
+        if state[i] == key:
+            state[i] = abbrev_dict[key]
+
+
 
 with open(output_file, 'w', newline='') as csvfile:
 
@@ -68,15 +130,6 @@ with open(output_file, 'w', newline='') as csvfile:
 
 
 
-state= {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut",
-            "DE":"Delaware","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana",
-            "IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland",
-            "MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri",
-            "MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey",
-            "NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio",
-            "OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island","SC":"South Carolina",
-            "SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia",
-            "WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
 
 ### VERSION USING DICTIONARIES
 
